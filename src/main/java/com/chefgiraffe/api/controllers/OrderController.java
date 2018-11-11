1d9b420d4fc2447848f1966ddee0cc3101d45816
Christopher Budo <cb9595@gmail.com>
2018-11-10 19:44:40 -0500

    controllers interfacing with database properly

--- src/main/java/com/chefgiraffe/api/controllers/OrderController.java
+++ src/main/java/com/chefgiraffe/api/controllers/OrderController.java
@@ -1,20 +1,29 @@
 package com.chefgiraffe.api.controllers;
 
 import com.chefgiraffe.api.controllers.models.Order;
-import com.chefgiraffe.api.repositories.models.OrderStatus;
-import com.chefgiraffe.api.repositories.models.RestaurantOrder;
+import com.chefgiraffe.api.services.OrderService;
+import com.chefgiraffe.api.services.models.CreatedOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
+import java.util.Optional;
 
-@Controller
+@RestController
 public class OrderController {
+    private OrderService orderService;
+    @Value("${base_url}")
+    private String base_url;
+
+    @Autowired
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
 
     @GetMapping("/order/{id}")
     public ResponseEntity<Order> getOrder(@PathVariable("id") String id) {
@@ -27,11 +36,16 @@ public class OrderController {
     }
 
     @PostMapping("/order")
-    public ResponseEntity<Object> createId(Order order) {
-        RestaurantOrder restaurantOrderObject = new RestaurantOrder(order.getTableId(), OrderStatus.CREATED);
-        UriComponents builder = UriComponentsBuilder.fromUriString("")
-                .pathSegment("v1", "order", restaurantOrderObject.getId().toString()).build(true);
-        return ResponseEntity.created(builder.toUri()).build();
+    public ResponseEntity<Object> createId(@RequestBody Order order) {
+        Optional<CreatedOrder> createdOrder = orderService.create(order.getTableId(), order.getOrderItems());
+
+        if (createdOrder.isPresent()) {
+            UriComponents builder = UriComponentsBuilder.fromUriString(base_url)
+                    .pathSegment("v1", "order", createdOrder.get().getOrderId().toString()).build(true);
+            return ResponseEntity.created(builder.toUri()).build();
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
     }
 
 
--- src/main/java/com/chefgiraffe/api/controllers/TableController.java
+++ src/main/java/com/chefgiraffe/api/controllers/TableController.java
@@ -1,30 +1,39 @@
 package com.chefgiraffe.api.controllers;
 
+import com.chefgiraffe.api.controllers.models.Table;
 import com.chefgiraffe.api.repositories.OrderRepository;
 import com.chefgiraffe.api.repositories.TableRepository;
 import com.chefgiraffe.api.repositories.models.RestaurantOrder;
 import com.chefgiraffe.api.repositories.models.RestaurantTable;
+import com.chefgiraffe.api.services.RestaurantTableService;
+import com.chefgiraffe.api.services.models.CreatedTable;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-@Controller
+@RestController
 public class TableController {
-    TableRepository tableRepository;
-    OrderRepository orderRepository;
+    private TableRepository tableRepository;
+    private OrderRepository orderRepository;
+    private RestaurantTableService restaurantTableService;
+    @Value("${base_url}")
+    private String base_url;
+
 
     @Autowired
-    public TableController(TableRepository tableRepository, OrderRepository orderRepository) {
-        this.tableRepository = tableRepository;
+    public TableController(RestaurantTableService restaurantTableService, TableRepository tableRepository, OrderRepository orderRepository) {
+        this.restaurantTableService = restaurantTableService;
         this.orderRepository = orderRepository;
+        this.tableRepository = tableRepository;
     }
 
     @GetMapping("/tables")
@@ -32,6 +41,19 @@ public class TableController {
         List<RestaurantTable> restaurantTables = new ArrayList<>();
         tableRepository.findAll().forEach(restaurantTables::add);
         return new ResponseEntity<>(restaurantTables, HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    @PostMapping("/table")
+    public ResponseEntity<Object> createTable(@RequestBody Table table) {
+        Optional<CreatedTable> createdTable = restaurantTableService.create(table.getRestaurantId(), table.getFriendlyName(), table.getAvailableSeats());
+        if (createdTable.isPresent()) {
+            UriComponents builder = UriComponentsBuilder.fromUriString(base_url)
+                    .pathSegment("v1", "table", createdTable.get().getTableId().toString()).build(true);
+            return ResponseEntity.created(builder.toUri()).build();
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+
     }
 
     @GetMapping("/table/{id}")
