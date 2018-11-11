fa8f7b0dd49f17d9c20331b48fd300188f07a6a6
Taylor Brady <taylor.brady@8451.com>
2018-11-11 09:01:44 -0500

    updated mock table names

--- src/main/resources/db/migration/V2__mock_data.sql
+++ src/main/resources/db/migration/V2__mock_data.sql
@@ -2,16 +2,16 @@ insert into restaurant(id, name, created_time)
 values ('a5eea915-128e-40b0-963c-e61164750511','Darnell''s Darn Good!','2018-11-11');
 
 insert into restaurant_table(id, restaurant_id, friendly_name, available_seats)
-values ('127e65d0-13a0-4be2-9b5a-ed0635b9c5ce','a5eea915-128e-40b0-963c-e61164750511','table 0',5),
-       ('9ace77d1-fdc2-4272-9106-a5ce2cd96cc9','a5eea915-128e-40b0-963c-e61164750511','table 1',5),
-       ('fcff4911-1b2a-42e6-8590-e6cbecee2675','a5eea915-128e-40b0-963c-e61164750511','table 2',5),
-       ('986f4c43-24f6-4692-81ca-2f98ed34306c','a5eea915-128e-40b0-963c-e61164750511','table 3',3),
-       ('e17dd5be-ac44-4868-a0e8-9a8668798aa0','a5eea915-128e-40b0-963c-e61164750511','table 4',3),
-       ('f872d58a-eacd-4b4d-ad33-8e9b6a140b02','a5eea915-128e-40b0-963c-e61164750511','table 5',3),
-       ('49c40776-3811-4e36-b5dd-1ee074a79915','a5eea915-128e-40b0-963c-e61164750511','table 6',3),
-       ('f454634a-b744-4960-ab51-353c80d20fcc','a5eea915-128e-40b0-963c-e61164750511','table 7',8),
-       ('ab9c1206-dc9d-4a05-b0c6-eb14cd484272','a5eea915-128e-40b0-963c-e61164750511','table 8',8),
-       ('c7fd08b1-7f00-4faf-b6ad-8b32e73d35cb','a5eea915-128e-40b0-963c-e61164750511','table 9',8);
+values ('127e65d0-13a0-4be2-9b5a-ed0635b9c5ce','a5eea915-128e-40b0-963c-e61164750511','Darnell''s Table',5),
+       ('9ace77d1-fdc2-4272-9106-a5ce2cd96cc9','a5eea915-128e-40b0-963c-e61164750511','Front Patio',5),
+       ('fcff4911-1b2a-42e6-8590-e6cbecee2675','a5eea915-128e-40b0-963c-e61164750511','Back Balcony',5),
+       ('986f4c43-24f6-4692-81ca-2f98ed34306c','a5eea915-128e-40b0-963c-e61164750511','City View 1',3),
+       ('e17dd5be-ac44-4868-a0e8-9a8668798aa0','a5eea915-128e-40b0-963c-e61164750511','City View 2',3),
+       ('f872d58a-eacd-4b4d-ad33-8e9b6a140b02','a5eea915-128e-40b0-963c-e61164750511','City View 3',3),
+       ('49c40776-3811-4e36-b5dd-1ee074a79915','a5eea915-128e-40b0-963c-e61164750511','Center Restaurant 1',3),
+       ('f454634a-b744-4960-ab51-353c80d20fcc','a5eea915-128e-40b0-963c-e61164750511','Center Restaurant 2',8),
+       ('ab9c1206-dc9d-4a05-b0c6-eb14cd484272','a5eea915-128e-40b0-963c-e61164750511','Center Restaurant 3',8),
+       ('c7fd08b1-7f00-4faf-b6ad-8b32e73d35cb','a5eea915-128e-40b0-963c-e61164750511','VIP',8);
 
 insert into restaurant_menu(id, restaurant_id, name)
 values ('2ea2f2c9-bfc3-4ec3-b568-82c21130fc10','a5eea915-128e-40b0-963c-e61164750511','Darnell''s Delicious Apps'),
