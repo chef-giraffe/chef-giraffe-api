02fd807902e4fa87d60e9327f1425a11d5d7b48d
Taylor Brady <taylor.brady@8451.com>
2018-11-10 19:50:46 -0500

    added the ability to update an existing order.

--- src/main/java/com/chefgiraffe/api/services/impl/OrderServiceImpl.java
+++ src/main/java/com/chefgiraffe/api/services/impl/OrderServiceImpl.java
@@ -4,10 +4,12 @@ import com.chefgiraffe.api.repositories.ItemRepository;
 import com.chefgiraffe.api.repositories.OrderRepository;
 import com.chefgiraffe.api.repositories.TableRepository;
 import com.chefgiraffe.api.repositories.models.RestaurantOrder;
-import com.chefgiraffe.api.repositories.models.OrderStatus;
 import com.chefgiraffe.api.repositories.models.RestaurantTable;
 import com.chefgiraffe.api.services.OrderService;
 import com.chefgiraffe.api.services.models.CreatedOrder;
+import com.chefgiraffe.api.services.models.OrderStatus;
+import com.chefgiraffe.api.services.models.OrderUpdate;
+import com.chefgiraffe.api.services.models.UpdatedOrder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,17 +41,44 @@ public class OrderServiceImpl implements OrderService {
         Optional<RestaurantTable> table = tableRepository.findById(tableId);
         if (table.isPresent()) {
 
-            logger.debug("table {} found - creating new order", tableId.toString());
-            RestaurantOrder newRestaurantOrder = new RestaurantOrder(tableId, OrderStatus.CREATED);
+            logger.debug("table {} found - creating new order", table.get().getId().toString());
+            RestaurantOrder newRestaurantOrder =
+                    new RestaurantOrder(table.get().getId(), OrderStatus.CREATED.toString());
 
             itemRepository.findAllById(items).forEach(newRestaurantOrder::addItem);
 
             logger.info("saving new order for table {}", tableId.toString());
             RestaurantOrder savedRestaurantOrder = orderRepository.save(newRestaurantOrder);
 
-            return Optional.of(new CreatedOrder(savedRestaurantOrder.getId(), savedRestaurantOrder.getRestaurantTableId(), savedRestaurantOrder.getMenuItems().size()));
+            return Optional.of(new CreatedOrder(savedRestaurantOrder.getId(),
+                                                savedRestaurantOrder.getRestaurantTableId(),
+                                                savedRestaurantOrder.getMenuItems().size()));
         } else {
             logger.warn("table {} not found", tableId.toString());
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public Optional<UpdatedOrder> update(OrderUpdate update) {
+
+        Optional<RestaurantOrder> order = orderRepository.findById(update.getOrderId());
+        if (order.isPresent()) {
+
+            RestaurantOrder existingOrder = order.get();
+
+            logger.debug("order {} found", existingOrder.getId().toString());
+
+            existingOrder.updateStatus(update.getUpdateStatus().toString());
+
+            RestaurantOrder savedUpdatedOrder = orderRepository.save(existingOrder);
+
+            return Optional.of(new UpdatedOrder(savedUpdatedOrder.getId(),
+                                                savedUpdatedOrder.getRestaurantTableId(),
+                                                OrderStatus.valueOf(savedUpdatedOrder.getOrderStatus())));
+
+        } else {
+            logger.warn("order {} not found", update.getOrderId().toString());
             return Optional.empty();
         }
     }
---
+++ src/main/java/com/chefgiraffe/api/services/models/UpdatedOrder.java
@@ -0,0 +1,17 @@
+package com.chefgiraffe.api.services.models;
+
+import java.util.UUID;
+
+public class UpdatedOrder extends Order{
+
+    private OrderStatus newStatus;
+
+    public UpdatedOrder(UUID orderId, UUID tableId, OrderStatus newStatus) {
+        super(orderId, tableId);
+        this.newStatus = newStatus;
+    }
+
+    public OrderStatus getNewStatus() {
+        return newStatus;
+    }
+}
